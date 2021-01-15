88eac63e3f5a768c26e16dc1252358c3806d03e8
sara8998 <77490883+sara8998@users.noreply.github.com>
2021-01-15 20:42:17 +0300

    Add files via upload

---
+++ review/birthyear.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter your age : ");
+            string age = Console.In.ReadLine();
+            int Iage = int.Parse(age);
+            DateTime today = DateTime.Today;
+            int datebirth = today.Year - Iage;
+            Console.WriteLine("your birthyear " + datebirth + ".");
+            Console.ReadLine();
+        }
+    }
+}
